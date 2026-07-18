9953e37136cde72ae19911a0fa98c2d2c4eb65be
Somay <somaynaik49@gmail.com>
2026-07-19 00:41:43 +0530

    Default theme set to light; user can still toggle dark mode manually

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light">
       <AuthProvider>
         <TooltipProvider>
           <Toaster />
